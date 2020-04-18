bab744030d4c090df3cef238d3be81d8e9cf4516
Zack Oldham <zoo150030@utdallas.edu>
2020-04-18 12:43:33 -0500

    final edits/fully trained model
    -- Added functionality for debug vs training mode in nn_offline.py
    -- Trained a model on all currently available data in team_3_offline database

--- Offline/nn_offline.py
+++ Offline/nn_offline.py
@@ -17,6 +17,7 @@ from influxdb import DataFrameClient as df_client
 from FaultDetectNet import FaultDetectNet
 from sklearn.metrics import accuracy_score
 from pywt import downcoef
+import sys
 
 
 client = df_client(host='localhost', port=8086)
@@ -26,12 +27,13 @@ write_client = df_client(host='localhost', port=8086)
 write_client.switch_database('team_3_test_detect')
 
 
-
+DEBUG = False
+MW_MAX = 0
 MW = 0
 SAMPLE_SIZE = 3000  # one input contains 3000 points -- this will be pre-processed with downcoef
 BATCH_SIZE = 10  # 10 samples at a time
-TRAIN_CUT = 150
-TEST_CUT = 200
+TRAIN_CUT = 0
+TEST_CUT = MW_MAX
 
 
 # Perform partial discrete wavelet transform on gs data to obtain approximate coefficients (condense sample size from 3000 to 30)
@@ -83,60 +85,67 @@ def cons(X, Y):
 
 
 
-
-def write_detect(y_true, y_pred, conf):
-    
-    for yt, yp, c in zip(y_true, y_pred, conf):
-        data = {}
-        data['actual'] = [yt.item()]
-        data['predicted'] = [yp]
-        data['confidence'] = [c]
-        data['time'] = datetime.now()
-        data = pd.DataFrame(data)
-        data = data.set_index('time')
-        write_client.write_points(data, 'detect')
-
-
-
-
 # train offline classifier and save the model for later use
 def main():
-    global MW
+    global MW, DEBUG, MW_MAX, TRAIN_CUT 
+       
+    if len(sys.argv) < 2:
+        print('Usage: python3 nn_offline.py -<train | debug> <max mw>')
+    elif sys.argv[1] == '-debug':
+        DEBUG = True
+    elif sys.argv[1] == '-train':
+        DEBUG = False
+    else:
+        print('ERR: Invalid argument; options are -debug or -train')    
+    
+    
+    MW_MAX = int(sys.argv[2])
+
     offline_nn = FaultDetectNet()
 
-    print('Training...')
-    while MW < TRAIN_CUT:
-        print('MW:', MW)
-        X_trn, Y_trn = get_batch(TRAIN_CUT)
-        offline_nn.fit(X_trn, Y_trn)
+    if DEBUG:
+        TRAIN_CUT = int(0.8 * MW_MAX)
+        print('Training...')
+        while MW < TRAIN_CUT:
+            print('MW:', MW)
+            X_trn, Y_trn = get_batch(TRAIN_CUT)
+            offline_nn.fit(X_trn, Y_trn)
 
 
-    Y_pred = []
-    Y_true = []
+        Y_pred = []
+        Y_true = []
 
-    print('Testing...')
-    while MW < TEST_CUT:
-        print('MW:', MW)
-        X_tst, Y_tst = get_batch(TEST_CUT)
-        Y_true = cons(Y_true, np.array(Y_tst).astype(int))
-        offline_nn.eval()
-        yp, conf = offline_nn(X_tst, eval=True)
-        Y_pred = cons(Y_pred, yp)
-        
-        # write to detect
-        write_detect(Y_true, yp, conf)
+        print('Testing...')
+        while MW < TEST_CUT:
+            print('MW:', MW)
+            X_tst, Y_tst = get_batch(TEST_CUT)
+            Y_true = cons(Y_true, np.array(Y_tst).astype(int))
+            offline_nn.eval()
+            yp, conf = offline_nn(X_tst, eval=True)
+            Y_pred = cons(Y_pred, yp)
+         
+         
+        Y_pred = np.array(Y_pred).flatten().astype(int)
 
-
-    Y_pred = np.array(Y_pred).flatten().astype(int)
-
-    print('Predicted Values:', Y_pred)
-    print('True Values:', Y_true)
+        print('Predicted Values:', Y_pred)
+        print('True Values:', Y_true)
 
 
-    acc = accuracy_score(Y_true, Y_pred)
-    print('Accuracy: {0:4.4f}'.format(acc))
+        acc = accuracy_score(Y_true, Y_pred)
+        print('Accuracy: {0:4.4f}'.format(acc))
 
-    torch.save({'model_state_dict': offline_nn.state_dict(), 'optimizer_state_dict': offline_nn.optimizer.state_dict(), 'loss': offline_nn.loss}, 'offline_nn.tar')
+
+
+
+    else:
+        TRAIN_CUT = MW_MAX
+
+        while MW < TRAIN_CUT:
+            print('MW:', MW)
+            X_trn, Y_trn = get_batch(TRAIN_CUT)
+            offline_nn.fit(X_trn, Y_trn)
+
+        torch.save({'model_state_dict': offline_nn.state_dict(), 'optimizer_state_dict': offline_nn.optimizer.state_dict(), 'loss': offline_nn.loss}, 'offline_nn.tar')
 
 
 if __name__ == '__main__':
